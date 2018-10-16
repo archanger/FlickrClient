12b4769b55dffd054def2c464bb9b1e01c1184e7
Kirill Chuyanov <chuyanov.k@gmail.com>
2018-10-16 21:37:06 +0300

    Initial

---
+++ FlickrClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrClient
+//
+//  Created by Кирилл Чуянов on 16/10/2018.
+//  Copyright © 2018 Kirill Chuyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
